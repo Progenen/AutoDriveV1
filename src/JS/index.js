bd8d4bfbe07065782a97a14bece202bae05a2b7d
Stanislav <stanislav_2010@icloud.com>
2024-08-31 00:24:11 +0500

    dsds

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,5 +1,5 @@
 import Swiper from "swiper";
-import { Navigation, Pagination, FreeMode } from "swiper/modules";
+import { Navigation, Pagination, FreeMode, Grid } from "swiper/modules";
 import { EffectFade } from 'swiper/modules';
 import { Fancybox } from "@fancyapps/ui";
 
@@ -26,7 +26,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 type: "bullets"
             },
             breakpoints: {
-                
+
                 992: {
                     navigation: {
                         nextEl: ".slider__arrow--next",
@@ -80,16 +80,16 @@ document.addEventListener('DOMContentLoaded', function () {
             const singleCalcRes = document.querySelector(".model-single-calc__results");
             const singleCalcMdColBtn = document.querySelector(".model-single-calc__col--md-btn");
             const singleCalcMdColRes = document.querySelector(".model-single-calc__col--md-results");
-    
+
             singleCalcMdColBtn.append(singleCalcBtn);
             singleCalcMdColRes.append(singleCalcRes);
         }
-     }
+    }
 
     if (window.innerWidth <= 992) {
         const headerColUi = document.querySelector(".header__col--ui");
         const headerRow = header.querySelector(".row");
-        
+
         headerRow.append(headerColUi);
 
         if (document.querySelector(".credit__form")) {
@@ -100,11 +100,21 @@ document.addEventListener('DOMContentLoaded', function () {
             creditRowBottom.append(creditRes);
             creditRowBottom.append(creditBtn);
         }
+        
+
+        if (document.querySelector("[data-md-bg]")) {
+            const dataBgItems = document.querySelectorAll("[data-md-bg]");
+
+            dataBgItems.forEach(el => {
+                console.log(el.getAttribute("data-md-bg"));
+                el.style.backgroundImage = `url(${el.getAttribute("data-md-bg")})`; 
+            })
+        }
     }
 
     if (window.innerWidth <= 768) {
         const headerMenu = header.querySelector(".header__menu");
-        const headerCallBtn = header.querySelector(".header__call"); 
+        const headerCallBtn = header.querySelector(".header__call");
         const headerTop = header.querySelector(".header__top");
         const mobmenuWrapper = document.querySelector(".mob-menu-wrapper");
         const mobmenuBody = document.querySelector(".mob-menu__body");
@@ -137,10 +147,20 @@ document.addEventListener('DOMContentLoaded', function () {
             const modelSingleCompUi = document.querySelectorAll(".model-single-complect__item-ui");
             const modelSingleCompInfo = document.querySelectorAll(".model-single-complect__item-info");
 
-            for(let i = 0; i <= modelSingleCompOption.length - 1; i++) {
+            for (let i = 0; i <= modelSingleCompOption.length - 1; i++) {
                 console.log(i);
                 modelSingleCompOption[i].insertBefore(modelSingleCompInfo[i], modelSingleCompUi[i])
             }
+        }
+
+        
+        if (document.querySelector("[data-sm-bg]")) {
+            const dataBgItems = document.querySelectorAll("[data-sm-bg]");
+
+            dataBgItems.forEach(el => {
+                console.log(el.getAttribute("data-sm-bg"));
+                el.style.backgroundImage = `url(${el.getAttribute("data-sm-bg")})`; 
+            })
         }
     }
 
@@ -154,7 +174,7 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 
-    if(document.querySelector(".model-single-calc")) {
+    if (document.querySelector(".model-single-calc")) {
         const singleCalcBtn = document.querySelector(".model-single-calc__btn");
         const singleCalcForm = document.querySelector(".model-single-calc__form");
 
@@ -162,26 +182,52 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     if (document.querySelector(".spoiler")) {
-        $(".spoiler").each(function(i, el) {
+        $(".spoiler").each(function (i, el) {
             $(el).on("click", () => {
                 $(el).toggleClass("active");
-                $(el).next().slideToggle(100)     
+                $(el).next().slideToggle(100)
             })
         })
     }
 
     Fancybox.bind("[data-fancybox]", {
         // Your custom options
-      });
+    });
 
-      
+
+    if (document.querySelector(".banks__slider")) {
+        const banksSlider = new Swiper(".banks__slider", {
+            modules: [Grid, Navigation],
+            slidesPerView: 2,
+            grid: {
+                rows: 5,
+                fill: "row",
+            },
+            navigation: {
+                nextEl: ".slider__arrow--next",
+                prevEl: ".slider__arrow--prev"
+            },
+
+            breakpoints: {
+                992: {
+                    slidesPerView: 4,
+                    spaceBetween: 40,
+                    grid: {
+                        rows: 3,
+                        fill: "row",
+                    },
+                },
+            }
+        })
+    }
+
 });
 
-let deadline; 
+let deadline;
 let contribution;
 let summ = 6000000;
 let res;
-$('.input-slider__range--deadline').ionRangeSlider({ 
+$('.input-slider__range--deadline').ionRangeSlider({
     values: ["2", "6", "12", "24", "36", "48", "60", "72", "84", "96"],
     hide_min_max: true,
     hide_from_to: true,
@@ -196,12 +242,12 @@ $('.input-slider__range--deadline').ionRangeSlider({
         }
     }
 })
-$(".input-slider__range--percent").ionRangeSlider({ 
+$(".input-slider__range--percent").ionRangeSlider({
     values: ["0", "10", "20", "30", "40", "50", "60", "70", "80"],
     hide_min_max: true,
     hide_from_to: true,
     onChange: (el) => {
-        contribution =  Math.round(summ * (el.from_value / 100))
+        contribution = Math.round(summ * (el.from_value / 100))
         el.input.parent().parent().find(".input-slider__res--percent .input-slider__res-val").text(contribution);
         if (deadline != undefined && contribution != undefined && summ != undefined) {
             res = (summ - contribution) / deadline;
